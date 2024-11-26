5e2bc6897b3e8113de7bf78f5c414a86a712d961
Sten Relve <sten.relve@telia.ee>
2024-11-26 13:53:51 +0200

    created search func

--- prooviulesanne/views.py
+++ prooviulesanne/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'business_registry/home.html')
+
+def search(request):
+    search_info = request.GET.get("search", "")
+    print("KATSE", search_info)
+    context = {
+        "search_info": search_info,
+    }
+    return render(request, 'business_registry/company_search.html', context)
