177efe94f71fcb8eaf5607431c2197c70a483c80
Sten Relve <sten.relve@telia.ee>
2024-11-28 09:22:43 +0200

    created company_details endpoint

--- business_registry/models.py
+++ business_registry/models.py
@@ -12,6 +12,9 @@ class Company(models.Model):
     registration_code = models.CharField(max_length=7, unique=True)
     established_date = models.DateField()
     total_capital = models.PositiveIntegerField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -23,6 +26,9 @@ class Shareholder(models.Model):
     identification_code = models.IntegerField(unique=True, null=True, blank=True)
     entity_name = models.CharField(max_length=100, null=True, blank=True)
     entity_reg_code = models.CharField(max_length=7, unique=True, null=True, blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         if self.type == "person":
@@ -34,6 +40,9 @@ class CompanyShareholder(models.Model):
     shareholder = models.ForeignKey(Shareholder, related_name="companies", on_delete=models.CASCADE)
     ownership_share = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     is_founder = models.BooleanField(default=False)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.shareholder} - {self.company}"
