cb36d5978034ca30518bba5da47a1f21461795b0
Sten Relve <sten.relve@telia.ee>
2024-12-01 11:16:31 +0200

    company create form update

--- business_registry/forms.py
+++ business_registry/forms.py
@@ -2,13 +2,29 @@
 from django import forms
 from .models import Company, Shareholder
 from django.utils.timezone import now
-from django.core.validators import MaxLengthValidator
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 class CompanyCreationForm(forms.ModelForm):
-    established_date = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
-    code = forms.CharField(widget=forms.NumberInput(attrs={"type": "number","maxlength": "7"}),
+    established_date = forms.DateField(widget=forms.DateInput(attrs={"type": "date",
+                                                                     "class": "form-control",}),
+                                                              help_text="Date for the establishment of the company.")
+    code = forms.CharField(
+                            widget=forms.NumberInput(attrs={"type": "number",
+                                                           "class": "form-control",
+                                                           "maxlength": "7"}),
                               validators=[MaxLengthValidator(7,
-                                message="The registration code must not exceed 7 numbers. You entered %(show_value)s number.")])
+                                message="The registration code must not exceed 7 numbers. You entered %(show_value)s number.")],
+                              help_text="The registration code must be exactly 7 numbers."
+                              )
+
+    name = forms.CharField(
+                            widget=forms.TextInput(attrs={"type": "text", "class": "form-control"}),
+                            validators=[
+                                MaxLengthValidator(100, message="The name must not exceed 100 characters. You entered %(show_value)s character."),
+                                MinLengthValidator(3, message="The name must contain at least 3 characters. You entered %(show_value)s character.")
+                            ],
+                            help_text="The name must contain at least 3 characters and not exceed 100 characters."
+                        )
     class Meta:
         model = Company
         fields = "__all__"
@@ -16,8 +32,9 @@ class CompanyCreationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        for _, field in self.fields.items():
-            field.widget.attrs["class"] = "form-control"
+        for name, field in self.fields.items():
+            if not name == "established_date" or not name == "code" or not name == "name":
+                field.widget.attrs["class"] = "form-control"
             
     def clean_code(self):
         code = self.cleaned_data['code']
@@ -33,6 +50,11 @@ class CompanyCreationForm(forms.ModelForm):
         if established_date > current_date:
             raise forms.ValidationError("The establishment date cannot be in the future.")
         return established_date
+    
+    def clean_total_capital(self):
+        total_capital = self.cleaned_data.get("total_capital")
+        if not total_capital >= 2500:
+            raise forms.ValidationError("The total capital must be at least 2500 €.")
         
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=100, min_length=1, help_text="Search by name or registry code.")
